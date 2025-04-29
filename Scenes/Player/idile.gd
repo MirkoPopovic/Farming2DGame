d8db8c9a4fc563fc8ed9630b45d79ff368157d61
Mirko <mirkopopovic21@gmail.com>
2025-04-29 20:09:56 +0200

    new player movment scripts

---
+++ Scenes/Player/idile.gd
@@ -0,0 +1,43 @@
+extends NodeState
+
+@warning_ignore("unused_signal")
+
+
+@export var player: Player
+@export var animated_sprite_2d: AnimatedSprite2D
+
+var direction: Vector2
+
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	#Calls func that return vector2 based on input from scripts/game_input_event.dg
+	direction = GameInputEvents.movement_input()
+	
+	if player.player_direction == Vector2.UP:
+		animated_sprite_2d.play("idile_up")
+	elif player.player_direction == Vector2.DOWN:
+		animated_sprite_2d.play("idile_down")
+	elif player.player_direction == Vector2.LEFT:
+		animated_sprite_2d.play("idile_left")
+	elif player.player_direction == Vector2.RIGHT:
+		animated_sprite_2d.play("idile_right")
+	else:
+		animated_sprite_2d.play("idile_down")
+
+
+func _on_next_transitions() -> void:
+	GameInputEvents.movement_input()
+	
+	if GameInputEvents.is_movment_input():
+		transition.emit("walk")
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	animated_sprite_2d.stop()
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,36 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
-var speed: int = 100
-var motion = Vector2.ZERO
-
-func _ready():
-	Global.set_player_reference(self)
-
-func _physics_process(delta):
-	var motion = Vector2.ZERO  
-
-	if Input.is_action_pressed("ui_right"):
-		motion.x += 1
-		$AnimatedSprite2D.play("walk_right")
-	elif Input.is_action_pressed("ui_left"):
-		motion.x -= 1
-		$AnimatedSprite2D.play("walk_left")
-	elif Input.is_action_pressed("ui_down"):
-		motion.y += 1
-		$AnimatedSprite2D.play("walk_down")
-	elif Input.is_action_pressed("ui_up"):
-		motion.y -= 1
-		$AnimatedSprite2D.play("walk_up")
-	else:
-		$AnimatedSprite2D.play("idile_front")
-
-	motion = motion.normalized() * speed  
-	velocity = motion
-	move_and_slide()
-
-	
-	var screen_size = get_viewport_rect().size
-	var margin = 16  
-
-	global_position.x = clamp(global_position.x, margin, screen_size.x - margin)
-	global_position.y = clamp(global_position.y, margin, screen_size.y - margin)
+var player_direction: Vector2
